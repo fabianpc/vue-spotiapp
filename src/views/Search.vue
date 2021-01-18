267ec0dcb5273eb1d0d5a11e87ee8e2c2ca4afc9
Fabian Palacios <fabianpc@gmail.com>
2021-01-18 18:54:23 -0500

    incorporar estilo general y nueva vista

---
+++ src/views/Search.vue
@@ -0,0 +1,22 @@
+<template>
+<div class="row">
+    <div class="col">
+        <input v-model="termino" v-on:keyup="buscar()" type="text" class="form-control" placeholder="Buscar artista...">
+    </div>
+</div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      termino: ""
+    }
+  },
+  methods: {
+    buscar: function() {
+      console.log(this.termino)
+    }
+  }
+}
+</script>
